0c4a0ae9c7a9f10db73eb0c7897753546c713022
Pedro Couto <2191265@my.ipleiria.pt>
2021-11-03 21:00:05 +0000

    Created Relations Between Models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,6 +10,14 @@ class Category extends Model
     use HasFactory;
 
     protected $table = 'categories';
-    protected $primaryKey = 'code';
-    protected $keyType = 'string';
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class, 'category_id', 'id');
+    }
+
+    public function vcard()
+    {
+        return $this->belongsTo(VCard::class, 'vcard', 'phone_number');
+    }
 }
--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -8,4 +8,34 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     use HasFactory;
+
+    public function payment_type()
+    {
+        return $this->belongsTo(PaymentType::class, 'payment_type', 'code');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
+
+    public function vcard()
+    {
+        return $this->belongsTo(VCard::class, 'vcard  ', 'phone_number');
+    }
+
+    public function vcardMirrored()
+    {
+        return $this->belongsTo(VCard::class, 'pair_vcard  ', 'phone_number');
+    }
+
+    public function transactionsPair()
+    {
+        return $this->hasOne(Transaction::class, 'pair_transaction', 'id');
+    }
+
+    public function transactionsPair2()
+    {
+        return $this->belongsTo(Transaction::class, 'pair_transaction', 'id');
+    }
 }
